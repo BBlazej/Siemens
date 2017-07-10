f75bf8bb4152ca1dcf8128de57a9e2f4b6e4780d
BBlazej <bbajzik@gmail.com>
2017-07-10 17:12:42 +0200

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::array<int, 5> arr = { 3, 4, 1, 5, 2 };
 	std::sort(std::begin(arr), std::end(arr));
-	std::sort(std::begin(arr), std::end(arr),
+	std::sort, std::end(arr),
 		std::greater<int>{});
-	cout << "all is fine"
+	cout << "all is not fine";
 }
